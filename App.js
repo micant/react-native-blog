11939e6eb1d565b8c7f11d6bdb71b15bf3d1213c
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-27 15:20:25 +1000

    added create screen navigator config

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from './src/context/BlogContext';
 import IndexScreen from './src/screens/IndexScreen';
 import ShowScreen from './src/screens/ShowScreens';
-import { Provider } from './src/context/BlogContext';
+import CreateScreen from './src/screens/CreateScreen';
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
-    Show: ShowScreen
+    Show: ShowScreen,
+    Create: CreateScreen
   }, 
   {
     initialRouteName: 'Index',
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = () => {
+    return (
+        <View>
+            <Text>Create</Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({});
+
+export default CreateScreen;
