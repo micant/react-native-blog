ba95a6792cad56ff682ee6091ac641201cbd6306
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-27 13:23:56 +1000

    navigate to blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,27 +3,29 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
-    return (    
+    return (
         <View>
             <Button title='Add Post' onPress={addBlogPost} />
             <FlatList
-                data={state} 
-                keyExtractor={(blogPost) => blogPost.id}    
+                data={state}
+                keyExtractor={(blogPost) => blogPost.id}
                 renderItem={({ item }) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>{item.title} - {item.id}</Text>
-                            <TouchableOpacity onPress={()=> deleteBlogPost(item.id)}>
-                                <Feather style={styles.icon} name="trash" />
-                            </TouchableOpacity> 
-                        </View>
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.icon} name="trash" />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     );
                 }}
             />
-        </View> 
+        </View>
     );
 };
 
