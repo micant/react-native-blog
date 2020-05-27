bf2d50655d5215292866e27c895597c075b41f02
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-27 13:13:56 +1000

    show blog screen with stack navigator config

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreens';
 import { Provider } from './src/context/BlogContext';
 
-const navigator = createStackNavigator({
-  Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
-  } 
-});
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+    Show: ShowScreen
+  }, 
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: 
+    {
+      title: 'Blogs'
+    } 
+  }
+);
 
 const App = createAppContainer(navigator);
 
