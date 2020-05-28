afa6ddf15004e003cff6e12ee58055a1751cc265
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-28 11:30:03 +1000

    added blog content to show screen, plus edit button to nav options

--- src/screens/ShowScreens.js
+++ src/screens/ShowScreens.js
@@ -1,19 +1,30 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context);
     
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
-    console.log(navigation.getParam('id'));
     return ( 
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     );
 };
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: (() =>
+            <TouchableOpacity  onPress={() => navigation.navigate('Edit')}>
+                <Feather name="edit" style={{marginRight: 15}} size={25} />
+            </TouchableOpacity>    
+        )
+    };
+}
 
 const styles = StyleSheet.create({});
 
