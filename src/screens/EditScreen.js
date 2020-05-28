5305bac0a92e29e625e4e60855a81f0ca47e70ed
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-28 11:38:39 +1000

    added edit screen plus routing

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const EditScreen = () => {
+    return (
+        <View> 
+            <Text>Edit Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
